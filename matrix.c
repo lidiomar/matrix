106edc6d1a7e432d09e95d33840045ea4306d58f
Lidiomar Fernando dos Santos Machado <fernandoMachado@Mac-mini-de-Lidiomar.local>
2018-08-26 23:47:54 -0300

    correcao

--- matrix.c
+++ matrix.c
@@ -4,36 +4,60 @@
 int** preencheMatriz(int linhas, int colunas, int** matriz);
 void mostraMatriz(int linhas, int colunas, int** matriz);
 int** alocaMatriz(int linhas, int colunas);
-int** multiplica(int linhas, int colunas, int **matriz_1, int **matriz_2, int **matriz_3);
+int** multiplica(int linhas, int colunas, int colunas2, int **matriz_1, int **matriz_2, int **matriz_3);
 
 int main(int argc, char **argv) {
-
-	int linhas = 2;
-	int colunas = 2;
+	int linhas;
+	int colunas;
+	int linhas2;
+	int colunas2;
 	int **matriz;
 	int **matriz_2;
 	int **matriz_3;
 
+	printf("Informe o número de linhas da matriz 1:\n");
+	scanf("%d",&linhas);
+
+	printf("Informe o número de colunas da matriz 1:\n");
+	scanf("%d",&colunas);
+	
+	printf("Informe o número de linhas da matriz 2:\n");
+	scanf("%d",&linhas2);
+
+	if(linhas2 != colunas) {
+		printf("O numero de colunas da matriz 1 deve ser igual ao número de linhas da matriz 2\n");
+	}
+
+	printf("Informe o número de colunas da matriz 2:\n");
+	scanf("%d",&colunas2);
+
+
+	printf("linhas %d\n", linhas);
+	printf("linhas* %d\n", linhas2);
+	printf("colunas %d\n", colunas);
+	printf("colunas* %d\n", colunas2);
+
 	matriz = alocaMatriz(linhas, colunas);
 	mostraMatriz(linhas, colunas, matriz);
 
-	matriz_2 = alocaMatriz(linhas, colunas);
-	mostraMatriz(linhas, colunas, matriz_2);
+	matriz_2 = alocaMatriz(linhas2, colunas2);
+	mostraMatriz(linhas2, colunas2, matriz_2);
 
 	matriz_3 = alocaMatriz(linhas, colunas);
-	matriz_3 = multiplica(linhas, colunas, matriz, matriz_2, matriz_3);
-	mostraMatriz(linhas, colunas, matriz_3);
+	matriz_3 = multiplica(linhas, colunas, colunas2, matriz, matriz_2, matriz_3);
+	mostraMatriz(linhas, colunas2, matriz_3);
 
 	return 0;
 }
 
 
-int** multiplica(int linhas, int colunas, int **matriz_1, int **matriz_2, int **matriz_3) {
+int** multiplica(int linhas, int colunas, int colunas2, int **matriz_1, int **matriz_2, int **matriz_3) {
 	int soma = 0;
 
 	for(int i=0; i<linhas; i++) {
-		for(int j=0;j<colunas;j++) {
+		for(int j=0;j<colunas2;j++) {
 			for(int k=0;k<colunas;k++) {
+				printf("%d %d %d %d\n",i,k,k,j );
 				soma += matriz_1[i][k] * matriz_2[k][j];
 			}
 			matriz_3[i][j] = soma;
@@ -46,23 +70,23 @@ int** multiplica(int linhas, int colunas, int **matriz_1, int **matriz_2, int **
 }
 
 int** alocaMatriz(int linhas, int colunas) {
-	int **matriz;
-	
-	matriz = malloc(linhas * sizeof(int *));
 
-	for(int i=0; i<colunas;i++) {
+	int **matriz;
+	matriz = malloc(linhas * sizeof(int *));
+	
+	for(int i=0; i<linhas;i++) {
 		matriz[i] = malloc(colunas * sizeof(int));
 	}
 
-	matriz = preencheMatriz(linhas, colunas, matriz);
-	return matriz;
+	return preencheMatriz(linhas, colunas, matriz);
+	
 }
 
 int** preencheMatriz(int linhas, int colunas, int** matriz) {
 	
-
 	for(int i=0;i<linhas;i++) {
 		for(int j=0; j<colunas; j++) {
+			
 			matriz[i][j] = rand()%100;
 		}
 	}
